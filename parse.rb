3d9bcac3b03523fe0f061bb43cd4b6f6652ce300
entcheva <christina.entcheva@gmail.com>
2016-12-12 17:43:49 -0500

    Done.

--- parse.rb
+++ parse.rb
@@ -1,1 +1,16 @@
 # parse the CSV into a sqlite3 database here!
+
+db = {:conn => SQLite3::Database.new("guests.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS guests")
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS guests (
+    year INTEGER,
+    occupation TEXT,
+    show TEXT,
+    type TEXT,
+    name TEXT
+    )
+    SQL
+
+DB[:conn].execute(sql)
